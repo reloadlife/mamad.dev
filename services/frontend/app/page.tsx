e89b5d7847491a13c89d60bdc0cb3bd30b21ed88
MamaD <me@mamad.dev>
2023-04-30 01:10:03 +0330

    ci

--- services/frontend/app/page.tsx
+++ services/frontend/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
-	return (
-		<div>
-				Hello, world!
-		</div>
-	);
+  return (
+    <div>
+      Updating ... :)
+      <br />
+      <a rel='noreferrer, nofollow, noindex' href='https://mamad-dev.pages.dev/'>Old Version !</a>
+    </div>
+  );
 }
