b1ec9f562885753a24e12b3f6d5ad37d520d8544
MamaD <me@mamad.dev>
2023-05-31 21:58:28 +0330

    added some stuff

--- services/frontend/app/page.tsx
+++ services/frontend/app/page.tsx
@@ -1,9 +1,42 @@
+'use client';
+
+import { Box, Text } from '@mantine/core';
+
 export default function Page() {
   return (
-    <div>
-      Updating ... :)
-      <br />
-      <a rel='noreferrer, nofollow, noindex' href='https://mamad-dev.pages.dev/'>Old Version !</a>
-    </div>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        background: 'url(/bg.jpg) no-repeat center center fixed',
+      }}
+    >
+      <Box
+        sx={{
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: 'linear-gradient(180deg, #000000 0%, #000000 27.6%, rgba(0, 0, 0, 0) 100%);',
+        }}
+      >
+        <Text<'h1'> component={'h1'} sx={{
+          fontSize: 40,
+          fontWeight: 400,
+          color: 'white',
+          
+        }}>
+          Mohammad Mahdi Afshar
+        </Text>
+        <Text<'h2'> component={'h2'} sx={{
+          marginTop: 5,
+          fontSize: 70,
+          fontWeight: 400,
+          color: '#D900FD',
+        }}>
+          Senior Software Engineer
+        </Text>
+      </Box>
+    </Box>
   );
 }
